c9e84020da57d476422246e39c97d167fd6a96ca
affif443 <80408741+affif443@users.noreply.github.com>
2021-05-22 10:26:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,96 +1,68 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var monkey , monkey_running
-var banana ,bananaImage, obstacle, obstacleImage
-var FoodGroup, obstacleGroup
-var score, ground
-var survivalTime
-
-function preload(){
-  
-  
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
-  
-  bananaImage = loadImage("banana.png");
-  obstaceImage = loadImage("obstacle.png");
- 
-  FoodGroup= new Group()
-  obstacleGroup= new Group()
-  
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
-
-
 
 function setup() {
-  createCanvas(670, 400);
-  score=0
-  survivalTime=0
-  
-  ground=createSprite(0,400,1500,10)
-  
-   monkey=createSprite(90,370,10,10)
-  monkey.addAnimation("monkey_running",monkey_running)
-  monkey.scale=0.1
-  
-  
-  
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  }
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
 function draw() {
-  background("green")
-  
-  if(keyDown("space")&&monkey.y >= 350){
-    monkey.velocityY=-10
-  }
-  monkey.velocityY = monkey.velocityY + 0.3
-  monkey.collide(ground)
-  
-  
-  ground.velocityX = -7 
- ground.x = ground.width/2;
-    
- if(World.frameCount%200===0){
-    fruits()
- }
-  
-  if(World.frameCount%300===0){
-    stones()
- }
-  
-  if(monkey.isTouching(FoodGroup)){
-     FoodGroup.destroyEach()
-    score=score+1
-      }
-  
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
  
- drawSprites()
-  fill("white") 
-  text("Score: "+ score, 500,50);
-  
-  fill("black")
-  var survivalTime=Math.round(getFrameRate()/1);
-  text("Survival Time: "+ survivalTime,350,50)
-  
 }
 
-function fruits(){
-  banana=createSprite(670,Math.round(random(170,230)),10,10)
-  banana.addImage(bananaImage)
-  banana.scale=0.1
-  banana.velocityX=-3
-  FoodGroup.add(banana)
+function keyPressed() {
+
+ if (keyCode===DOWN_ARROW) {
+
+    Matter.body.isStatic(packageBody,false)
+
+  }
+
 }
-
-function stones(){
-  obstacle=createSprite(670,380,10,10)
-  obstacle.addImage(obstaceImage)
-  obstacle.velocityX=-4
-  obstacle.scale=0.2
-  obstacleGroup.add(obstacle)
-}
-
-
-
-
 
 
 
